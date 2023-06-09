bada927ac7cecc437ea617767f89033149fb8e71
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2023-06-08 21:47:20 -0700

    revise processor_6502.png

--- src/main/java/com/alotofletters/uchip/content/machine/casing/CasingBlock.java
+++ src/main/java/com/alotofletters/uchip/content/machine/casing/CasingBlock.java
@@ -2,9 +2,11 @@ package com.alotofletters.uchip.content.machine.casing;
 
 import com.alotofletters.uchip.MicrochipBlockEntities;
 import com.alotofletters.uchip.MicrochipBlockStateProperties;
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
@@ -13,11 +15,17 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class CasingBlock extends HorizontalDirectionalBlock implements EntityBlock {
     public static final BooleanProperty HAS_BOARD = MicrochipBlockStateProperties.HAS_BOARD;
     public static final BooleanProperty ENABLED = BlockStateProperties.ENABLED;
+
+    private static final VoxelShape X_AXIS_AABB = Block.box(0, 0, 4, 16, 16, 12);
+
+    private static final VoxelShape Z_AXIS_AABB = Block.box(4, 0, 0, 12, 16, 16);
 
     public CasingBlock(Properties prop) {
         super(prop);
@@ -25,6 +33,15 @@ public class CasingBlock extends HorizontalDirectionalBlock implements EntityBlo
                 .setValue(FACING, Direction.NORTH)
                 .setValue(HAS_BOARD, false)
                 .setValue(ENABLED, false));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        if (p_60555_.getValue(FACING).getAxis() == Direction.Axis.X) {
+            return X_AXIS_AABB;
+        } else {
+            return Z_AXIS_AABB;
+        }
     }
 
     @Nullable
--- src/main/java/com/alotofletters/uchip/content/machine/casing/CasingBlockEntity.java
+++ src/main/java/com/alotofletters/uchip/content/machine/casing/CasingBlockEntity.java
@@ -1,22 +1,38 @@
 package com.alotofletters.uchip.content.machine.casing;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class CasingBlockEntity extends BlockEntity {
+    protected LazyOptional<IItemHandler> itemCapability;
+    protected LazyOptional<IEnergyStorage> energyCapability;
+
+    protected ItemStackHandler inventory;
+
     public CasingBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+
+        inventory = new ItemStackHandler(1) {
+            @Override
+            public int getSlotLimit(int slot) {
+                return 1;
+            }
+        };
+
+        itemCapability = LazyOptional.of(() -> inventory);
     }
 
-    @Override
-    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-        return super.getCapability(cap, side);
-    }
+//    @Override
+//    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+//        if (cap == ForgeCapabilities.ENERGY && side == getBlockState().getValue(CasingBlock.FACING).getOpposite()) {
+//            return itemCapability.cast();
+//        }
+//        return super.getCapability(cap, side);
+//    }
 }
